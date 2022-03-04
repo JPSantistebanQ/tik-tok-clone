3c6a8184d4038115d20c16b28584b73256ab6990
Juan Piero Santisteban Quiroz <jpsantq@gmail.com>
2022-03-03 23:46:04 -0500

    feat: progress propTypes

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,47 +1,59 @@
-import {supabase} from './supabase';
+import PropTypes, { InferProps } from 'prop-types';
+
+import { supabase } from './supabase';
 
 const prefix = import.meta.env.VITE_SUPABASE_STORAGE_URL;
 
 export const getVideos = async () => {
-    const {data, error} = await supabase
-        .from('videos')
-        .select(
-            `*, user:user_id (
+  const { data, error } = await supabase
+    .from('videos')
+    .select(
+      `*, user:user_id (
         avatar,
         username,
         id
       )`,
-        )
-        .order('created_at', {ascending: false});
+    )
+    .order('created_at', { ascending: false });
 
-    return [error, data];
+  return [error, data];
 };
 
-export const uploadVideo = async ({videoFile}) => {
-    const filename = window.crypto.randomUUID();
+export const uploadVideo = async ({ videoFile }) => {
+  const filename = window.crypto.randomUUID();
 
-    const {data, error} = await supabase.storage
-        .from('videos')
-        .upload(`uploads/${filename}.mp4`, videoFile);
+  const { data, error } = await supabase.storage
+    .from('videos')
+    .upload(`uploads/${filename}.mp4`, videoFile);
 
-    const file = data?.Key ? `${prefix}${data.Key}` : '';
-    return [error, file];
+  const file = data?.Key ? `${prefix}${data.Key}` : '';
+  return [error, file];
 };
 
-export const publishVideo = async ({videoSrc, description}) => {
-    const defaultAlbum =
-        'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/81fd9366b0f1841e1e6990492b5c9004~c5_720x720.jpeg?x-expires=1632060000&x-signature=lJ7S3aE3YqHr8WbhkwXNIasqkZo%3D';
-    const defaultSong = 'jpsantq songs';
+const PublishVideoPropTypes = {
+  videoSrc: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 
-    const {data, error} = await supabase.from('videos').insert([
-        {
-            user_id: '54f876df-fec6-4da7-a1ba-6210ea0f1760',
-            description,
-            albumCover: defaultAlbum,
-            songTitle: defaultSong,
-            src: videoSrc,
-        },
-    ]);
+type PublishVideoTypes = InferProps<typeof PublishVideoPropTypes>;
 
-    return [error, data];
+export const publishVideo: React.FC<PublishVideoTypes> = async ({
+  videoSrc,
+  description,
+}) => {
+  const defaultAlbum =
+    'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/81fd9366b0f1841e1e6990492b5c9004~c5_720x720.jpeg?x-expires=1632060000&x-signature=lJ7S3aE3YqHr8WbhkwXNIasqkZo%3D';
+  const defaultSong = 'jpsantq songs';
+
+  const { data, error } = await supabase.from('videos').insert([
+    {
+      user_id: '54f876df-fec6-4da7-a1ba-6210ea0f1760',
+      description,
+      albumCover: defaultAlbum,
+      songTitle: defaultSong,
+      src: videoSrc,
+    },
+  ]);
+
+  return [error, data];
 };
--- src/services/supabase.tsx
+++ src/services/supabase.tsx
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://bmpujepaekbusnhmkomn.supabase.co';
-const supabaseKey: string = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey: string = import.meta.env.VITE_SUPABASE_KEY as string;
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
